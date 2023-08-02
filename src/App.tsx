e376cc526845098d4cf8d315e36f475192e71c92
Matteo  Marengo <matteo.marengo@excentio.com>
2023-08-02 22:38:17 +0200

    fix su app

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,19 @@
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
-import BottomBar from "./Components/BottomBar/BottomBar";
 import { Routes, Route, HashRouter } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import Rent from "./Components/Rent/Rent";
 import Platforms from "./Components/Platforms/Platforms";
+import { EthereumInterface } from "./Interfaces/EthInterfaces";
+import Stats from "./Components/Stats/Stats";
 
-// interface RequestArguments {
-//   method: string;
-//   params?: unknown[] | object;
-// }
-// interface EthereumInterface {
-//   isConnected: () => boolean;
-//   isMetaMask: boolean;
-//   chainId: string;
-//   request: (args: RequestArguments) => Promise<unknown>;
-//   selectedAddress: string;
-// }
-
-// declare global {
-//   interface Window {
-//     ethereum: EthereumInterface;
-//   }
-// }
-
-// function getPublicAddress(ethereum: EthereumInterface) {
-//   if (ethereum.isMetaMask) {
-//     if (ethereum.selectedAddress != null) {
-//       return ethereum.selectedAddress;
-//     } else {
-//       return ethereum
-//         .request({
-//           method: "eth_requestAccounts",
-//         })
-//         .then((response) => {
-//           const eth_requestAccount = response as string[];
-//           return eth_requestAccount[0];
-//         })
-//         .catch((error) => console.log(error));
-//     }
-//   }
-// }
+declare global {
+  interface Window {
+    ethereum: EthereumInterface;
+  }
+}
 
 function App() {
-  // const [pubicAddress, setPublicAddress] = useState<string | undefined>();
-  // const ethereum = window.ethereum;
-
-  // useEffect(() => {
-  //   const response = getPublicAddress(ethereum);
-
-  //   if (typeof response === "string") {
-  //     setPublicAddress(response);
-  //   } else if (response) {
-  //     response.then((res) => {
-  //       if (typeof res === "string") setPublicAddress(res);
-  //       else {
-  //         console.log("errore nella chiamata");
-  //         setPublicAddress(undefined);
-  //       }
-  //     });
-  //   } else {
-  //     console.log("errore nella chiamata");
-  //     setPublicAddress(undefined);
-  //   }
-  // }, [ethereum.isConnected]);
-
   return (
     <HashRouter>
       <Navbar />
@@ -72,8 +21,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/rent" element={<Rent />} />
         <Route path="/platforms" element={<Platforms />} />
+        <Route path="/personal-area" element={<Stats />} />
       </Routes>
-      {/* <BottomBar /> */}
     </HashRouter>
   );
 }
