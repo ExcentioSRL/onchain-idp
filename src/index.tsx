be29032eae948eeab8198d90444e9bb291ac31e1
Matteo  Marengo <matteo.marengo@excentio.com>
2023-08-02 22:38:44 +0200

    css

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import "primeicons/primeicons.css";
 
 //theme
 import "primereact/resources/themes/viva-light/theme.css";
